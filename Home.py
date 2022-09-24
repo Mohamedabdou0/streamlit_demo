6aca040acaf78c33d6cddb52e6ef6f9a92ea08f4
Mohamedabdou0 <107133035+Mohamedabdou0@users.noreply.github.com>
2022-09-24 20:14:34 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd 
+st.title("Hello world")
+
+st.write("This is a test")
+
+st.markdown(''' 
+                - This is a test 
+                - This is a test
+                - This is a test
+
+
+''')
+
